e4cdd617d9569f76f0edd4fd430ab6455f19b79f
Nebojša Koturović <contact@kotur.me>
2024-03-15 18:05:30 +0100

    Save ollama related changes

--- plugins/user.lua
+++ plugins/user.lua
@@ -74,4 +74,43 @@ return {
       end,
     },
   },
+  {
+      "David-Kunz/gen.nvim",
+      dependencies = { "ldelossa/litee.nvim" },
+      event = "BufEnter",
+      config = function()
+        require("gen").setup({
+          model = "laser-dolphin-mixtral-2x7b-dpo:q5_0",
+          display_mode = "float", -- The display mode. Can be "float" or "split".
+          no_auto_close = true, -- Never closes the window automatically.
+      })
+      end,
+  },
+  -- {
+  --   "nomnivore/ollama.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim"},
+  --     event = "BufEnter",
+  --     config = function()
+  --       require("ollama").setup({
+  --        model = "laser-dolphin-mixtral-2x7b-dpo:q5_0",
+  --         url = "http://127.0.0.1:11434",
+  --         serve = {
+  --           on_start = false,
+  --           command = "ollama",
+  --           args = { "serve" },
+  --           stop_command = "pkill",
+  --           stop_args = { "-SIGTERM", "ollama" },
+  --         },
+  --         -- View the actual default prompts in ./lua/ollama/prompts.lua
+  --           -- prompts = {
+  --           --   Sample_Prompt = {
+  --           --     prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+  --           --     input_label = "> ",
+  --           --     model = "mistral",
+  --           --     action = "display",
+  --           --   }
+  --           -- }
+  --         })
+  --     end,
+  -- },
 }
