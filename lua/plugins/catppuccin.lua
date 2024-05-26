3d690c14ce050f0b0dbeec7b66c3ca26b3de1ecf
Nebojsa Koturovic <contact@kotur.me>
2024-05-26 13:35:14 +0200

    Commit changes

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,30 @@
+return {
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      -- term_colors = true,
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      --
+      -- term_colors = true,
+      -- transparent_background = true,
+      -- integrations = {
+      --   sandwich = false,
+      --   noice = true,
+      --   mini = true,
+      --   leap = true,
+      --   markdown = true,
+      --   neotest = true,
+      --   cmp = true,
+      --   overseer = true,
+      --   lsp_trouble = true,
+      --   rainbow_delimiters = true,
+      -- },
+    },
+  },
+}
